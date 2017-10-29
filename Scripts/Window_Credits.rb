6fb47aa36a2de9db48a45a4a0d75d7e053b4b52b
Nico Poblete <nlypoblet@gmail.com>
2017-10-29 12:24:09 -0700

    Updated bestiary

---
+++ Scripts/Window_Credits.rb
@@ -0,0 +1,29 @@
+#=======================================================================================
+# *** Credits Window
+#---------------------------------------------------------------------------------------
+#  This plugin calls up a window containing the credits (contributors, designers, etc.)
+# of the current project.
+#
+# * Version 1.0.1
+#
+# * Updated: 2017-10-29
+#
+# * Coded by: boaromayo/Quesada's Swan
+#
+# NOTE: If you are using this plugin for your projects (commercial or non-commercial), 
+# be sure to leave this comment visible or credit me (boaromayo or Quesada's Swan) 
+# in your projects.
+#=======================================================================================
+#===================================================================
+# ** New class: Window_Credits
+#===================================================================
+class Window_Credits < Window_Selectable
+  
+end
+
+#===================================================================
+# ** New class: Scene_Credits
+#===================================================================
+class Scene_Credits < Scene_Base
+
+end
